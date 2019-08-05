62222dc6cb0ba4160b56b0b84fad0e4c71f2e560
Royee Shemesh <royee@flexpert.co.il>
2019-08-05 13:44:45 +0300

    Initial import

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Hello world</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
